0d3e3a8663516e8c0f7c307182d8a3f5e77e9688
juny1902 <juny1905@naver.com>
2018-01-22 11:52:41 +0900

    180122

--- AdapterView/app/src/main/java/com/example/bit_user/adapterview/MainActivity.java
+++ AdapterView/app/src/main/java/com/example/bit_user/adapterview/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.bit_user.adapterview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -12,26 +14,37 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     EditText entryName;
-
+    Button btnAdd;
+    ListView listV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("리스트뷰 테스트");
         entryName = findViewById(R.id.entryListElement);
-
+        entryName.setSingleLine();
+        btnAdd = findViewById(R.id.btnAdd);
         final ArrayList<String> lists = new ArrayList<String>();
-        ListView listV = findViewById(R.id.listView1);
+        listV = findViewById(R.id.listView1);
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_single_choice, lists);
+                this, android.R.layout.simple_list_item_1, lists);
         listV.setAdapter(adapter);
-
-        listV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        entryName.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+                    btnAdd.callOnClick();
+                }
+                return false;
+            }
+        });
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 lists.add(entryName.getText().toString());
                 adapter.notifyDataSetChanged();
+                entryName.setText("");
             }
         });
 
